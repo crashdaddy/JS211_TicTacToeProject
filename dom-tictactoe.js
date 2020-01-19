0a461abb8cee5992e208709e389ca999f1dfa12d
crashdaddy <kennethscoggins@mail.com>
2020-01-19 09:26:31 -0600

    added wins counter

--- dom-tictactoe.js
+++ dom-tictactoe.js
@@ -4,7 +4,7 @@
 
 // 1. Read the code below and figure out the data flow
 // 2. Add in your code from the terminal app (check for win logic)
-// 3. Look for the @TODO, to fix
+// 3. Look for the @TODO][ to fix
 // next to each @TODO you will find tasks that need to be finished
 // 4. GET THIS GAME WORKING!!
 
@@ -15,27 +15,33 @@ let board = [
   ['','','']
 ];
 
+let wins = {
+  "X" : 0,
+  "O" : 0
+}
+
+
 // is called when a square is clicked. "this" = element here
 const handleClick = (element) => {
-  // check to see if the square clicked has anything in it, if not continue
+  // check to see if the square clicked has anything in it][ if not continue
   // this prevents an X being changed to an O
   if(!document.getElementById(element.id).innerHTML){
     addMarker(element.id)
     updateBoard(element.id)
     checkForWin()
-  }
+}
 }
 
 const addMarker = (id) => {
-  console.log(`We'll place a mark on square: ${id}`)
-  // @TODO, Mix & Match. 
+  console.log(`We'll place a mark on square: ${id}`);
+  // @TODO][ Mix & Match. 
   // You will need the following pieces:
   
   // = currentMarker
   // .getElementById(id)
   // document
   // .innerHTML 
-  
+  document.getElementById(id).innerHTML= currentMarker;
   // Arrange the above pieces into one a single line of code
   // to add an X or O to the board to the DOM so it can be scene on the screen.
 }
@@ -46,45 +52,76 @@ const updateBoard = (id) => {
   const row = parseInt(id.charAt(0))
   const column = parseInt(id.charAt(2)) 
 
-  console.log(`you clicked the sq at ${row} and ${column}`)
-  console.log(board)
-
-  // @TODO, Your code here: use the above information to change the board variable(array of arrays)
+  // @TODO][ Your code here: use the above information to change the board variable(array of arrays)
   // HINT: in your browser open up the dev tools -> console
+  board[row][column]= currentMarker;
+
 }
 
 const checkForWin = () => {
   // calls each checkForWin possibility and if any are true gives a page alert,
   if(horizontalWin() || verticalWin() || diagonalWin()) {
     // **BONUS** you could make the dismissal of this alert window reset the board...
-    window.alert(`Player ${currentMarker} won!`)
+    window.alert(`Player ${currentMarker} won!`);
+    message(currentMarker+" WINS!");
+    wins[currentMarker]++;
+    resetBoard();
   } else {
     // if no win, change the marker from X to O, or O to X for the next player.
     changeMarker()
-  }
+}
 }
 
 const horizontalWin = () => {
-  // @TODO, Your code here: to check for horizontal wins
+  // @TODO][ Your code here: to check for horizontal wins
+  
+  if (board[0][0]==="X" && board[0][1]==="X" && board[0][2]==="X") {return true;}
+  if (board[0][0]==="O" && board[0][1]==="O" && board[0][2]==="O") {return true;}
+  if (board[1][0]==="X" && board[1][1]==="X" && board[1][2]==="X") {return true;}
+  if (board[1][0]==="O" && board[1][1]==="O" && board[1][2]==="O") {return true;}
+  if (board[2][0]==="X" && board[2][1]==="X" && board[2][2]==="X") {return true;}
+  if (board[2][0]==="O" && board[2][1]==="O" && board[2][2]==="O") {return true;}
 }
 
 const verticalWin = () => {
-  // @TODO, Your code here: to check for vertical wins
+  // @TODO][ Your code here: to check for vertical wins
+  if (board[0][0]==="X" && board[1][0]==="X" && board[2][0]==="X") {return true;}
+  if (board[0][1]==="X" && board[1][1]==="X" && board[2][1]==="X") {return true;}
+  if (board[0][2]==="X" && board[1][2]==="X" && board[2][2]==="X") {return true;}
+  if (board[0][0]==="O" && board[1][0]==="O" && board[2][0]==="O") {return true;}
+  if (board[0][1]==="O" && board[1][1]==="O" && board[2][1]==="O") {return true;}
+  if (board[0][2]==="O" && board[1][2]==="O" && board[2][2]==="O") {return true;}
 }
 
 const diagonalWin = () => {
-  // @TODO, Your code here: to check for diagonal wins
+  // @TODO][ Your code here: to check for diagonal wins
+  if (board[0][0]==="X" && board[1][1]==="X" && board[2][2]==="X") {return true;}
+  if (board[0][2]==="X" && board[1][1]==="X" && board[2][0]==="X") {return true;}
+  if (board[0][0]==="O" && board[1][1]==="O" && board[2][2]==="O") {return true;}
+  if (board[0][2]==="O" && board[1][1]==="O" && board[2][0]==="O") {return true;}
 }
 
 const changeMarker = () => {
-  // ternary operator: if it's an X make it an O, if O make it an X
+  // ternary operator: if it's an X make it an O][ if O make it an X
   currentMarker = currentMarker === "X" ? "O" : "X"
+  message("It is "+currentMarker+"'s turn.");
+}
+
+function message(msg) {
+    document.getElementById("actionStatus").innerHTML=msg;
+}
+
+function score(){
+  document.getElementById("score").innerHTML="X: " + wins["X"] + " wins, O: " + wins["O"] + " wins";
+
 }
 
 const resetBoard = () => {
   // sanity check: this tells us the function is being called
   console.log("the board was cleared!")
-
+  currentMarker = "X";
+  //message("It is "+currentMarker+"'s turn.");
+  score();
   // collects all of the "td"s into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
   const squares = document.getElementsByTagName("TD")
   
@@ -94,8 +131,15 @@ const resetBoard = () => {
     squares[i].innerHTML = null
   }
   
-  // @TODO, Your code here: make sure to reset the array of arrays to empty for a new game
+  // @TODO][ Your code here: make sure to reset the array of arrays to empty for a new game
+  board = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+  ];
 }
+
+resetBoard();
 
 // **BONUSES**
 
@@ -103,4 +147,4 @@ const resetBoard = () => {
 // 2. Count number of wins for each player and display them
 // 3. Reset the number of wins
 // 4. Clear the board on alert window dismissal
-// 5. Add players names and display who wins, i.e. "Congrats Emily, you won with 0s!"
+// 5. Add players names and display who wins][ i.e. "Congrats Emily][ you won with 0s!"
